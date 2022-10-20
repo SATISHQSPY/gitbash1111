747c0cc9900f1f106089633c0e4efa819e7c322c
satish <satishklkrydlp@gmail.com>
2022-10-20 12:10:06 +0530

    start3

---
+++ src/test/java/gitBASH/Gitbash1111.java
@@ -0,0 +1,8 @@
+package gitBASH;
+
+public class Gitbash1111 {
+	public static void main(String[] args) {
+		System.out.println("----GITBASH----1111111---");
+	}
+	}
+
---
+++ target/test-classes/GITPRAC1.java
@@ -0,0 +1,6 @@
+
+public class GITPRAC1 {
+public static void main(String[] args) {
+	System.out.println("----GITBASH----1111111---");
+}
+}
